ed37ee1dc31bb0e742a3add28a745d4728244c27
paolacorr67 <paolacorr67@gmail.com>
2026-05-03 09:57:22 -0500

    Update documentation

--- build/_shared/ada-GLDAP67B.js
+++ build/_shared/ada-GLDAP67B.js
@@ -1,1 +1,1 @@
-import{a}from"/myst_assets_folder/_shared/chunk-KY2YZF4X.js";import"/myst_assets_folder/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/edaheart1/build/_shared/chunk-KY2YZF4X.js";import"/edaheart1/build/_shared/chunk-OZE3FFNP.js";export default a();
--- build/_shared/factor-MPF6W27P.js
+++ build/_shared/factor-MPF6W27P.js
@@ -1,1 +1,1 @@
-import{a}from"/myst_assets_folder/_shared/chunk-4VF2MTNC.js";import"/myst_assets_folder/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/edaheart1/build/_shared/chunk-4VF2MTNC.js";import"/edaheart1/build/_shared/chunk-OZE3FFNP.js";export default a();
